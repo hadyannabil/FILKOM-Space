c85a1c8e414800a0a662eb6009eba9e93283949a
hadyannabil <hadyannabilsk@gmail.com>
2026-03-31 12:55:24 +0700

    update halaman login & assets

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,8 +10,8 @@
     <div class="flex min-h-screen">
         <div class="hidden lg:flex w-1/2 bg-[#0A1628] items-center justify-center p-12 text-center">
             <div class="max-w-md">
-                <div class="w-20 h-20 bg-white/10 rounded-2xl mx-auto mb-8 flex items-center justify-center border border-white/20">
-                    <span class="text-white text-4xl">🏢</span>
+                <div class="w-20 h-20 bg-transparent rounded-2xl mx-auto mb-4 flex items-center justify-center">
+                    <img src="{{ asset('assets/login/gedung.webp') }}" alt="Icon Gedung" class="w-14 h-17 object-contain">
                 </div>
                 <h1 class="text-white text-5xl font-bold mb-4">FILKOM Space</h1>
                 <p class="text-gray-400 text-xl">University Room Booking System</p>
