ce59af2de71f16b496d9a85763940804fd923415
hadyannabil <hadyannabilsk@gmail.com>
2026-06-01 18:23:20 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,37 +6,33 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Models\Notification;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-
     public function boot(): void
     {
+        // Force HTTPS in production
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         View::composer('*', function ($view) {
             if (Auth::check()) {
-                // 1. Ambil jumlah yang belum dibaca
                 $count = Notification::where('user_id', Auth::id())
                                     ->where('is_read', false)
                                     ->count();
                 
-                // 2. Ambil 5 data notifikasi terbaru milik user tersebut
                 $notifs = Notification::where('user_id', Auth::id())
                                     ->orderBy('created_at', 'desc')
                                     ->take(5)
                                     ->get();
 
-                // 3. Kirim keduanya ke semua halaman Blade
                 $view->with('unreadCount', $count)
                     ->with('notifications', $notifs);
             } else {
